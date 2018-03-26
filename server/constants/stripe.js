3ec6938e2e5e844bd812f68f50f68a1608eb9af0
Jaylan Jones <jaylanjones421@gmail.com>
2018-03-26 18:44:42 -0500

    Message

--- server/constants/stripe.js
+++ server/constants/stripe.js
@@ -1,9 +1,6 @@
 const configureStripe = require("stripe");
 
-const STRIPE_SECRET_KEY =
-  process.env.NODE_ENV === "production"
-    ? "sk_live_MY_SECRET_KEY"
-    : "sk_test_eQQedpebWF61sYTbDJlaQG4m";
+const STRIPE_SECRET_KEY = "sk_test_eQQedpebWF61sYTbDJlaQG4m";
 
 const stripe = configureStripe(STRIPE_SECRET_KEY);
 
